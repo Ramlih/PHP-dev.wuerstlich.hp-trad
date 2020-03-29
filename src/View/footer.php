8f0727b731e6ae59eb54a803a9c93062ff208d31
Ramlih <h.kluschke@gmail.com>
2020-03-29 14:31:55 +0200

    Bereinigung von überflüssigen Kommentaren / Auskommentierungen

--- src/View/footer.php
+++ src/View/footer.php
@@ -7,24 +7,4 @@
 </ul>
 
 <?php
-/*
- * list ($width, $height, $type, $attr) = getimagesize("src/Medien/Images/Header/bgi-1000-300.jpg");
- *
- * echo "Image width " . $width;
- * echo "Image height " . $height;
- * echo "Image type " . $type;
- * echo "Attribute " . $attr;
- */
-
-?>
-
-<?php
-/*
- * $json = '{"id":1,"name":"foo","interest":["wordpress","php"]} ';
- *
- * $obj = json_decode($json);
- *
- * echo $obj->interest[0]; // prints php
- */
-?>
 
