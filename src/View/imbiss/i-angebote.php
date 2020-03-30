a71095cf92c0ac33401638cb66e1444d355a46de
Ramlih <hilmar.kluschke@wuerstlich.de>
2020-03-30 12:28:25 +0200

    1. Initial Commit für neues Memeber 2. Aktualisierung i-angebote.php

--- src/View/imbiss/i-angebote.php
+++ src/View/imbiss/i-angebote.php
@@ -31,6 +31,11 @@ switch ($idxFil) {
               <span class="font-petite-caps-yellow">Speisekarte</span>
             </a>
           </li>
+          <li>
+            <a href="src/Medien/Download/Wochenkarte30.03.20_RS.pdf">
+              <span class="font-petite-caps-yellow">Wochenkarte (<span>als pdf</span> <i class="fas fa-file-pdf"></i> )</span>
+            </a>
+          </li>
         </ul>
       </div>
       ';
@@ -71,7 +76,7 @@ switch ($idxFil) {
             </a>
           </li>
           <li>
-            <a href="src/Medien/Download/wochenkarte-23.03-27.03.pdf">
+            <a href="src/Medien/Download/Wochenkarte 30.03 bis 03.04_HDL.pdf">
               <span class="font-petite-caps-yellow">Wochenkarte (<span>als pdf</span> <i class="fas fa-file-pdf"></i> )</span>
             </a>
           </li>
